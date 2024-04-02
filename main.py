1eac087deff4231a9ecdcbdd7d7523e50570175f
vincentheynderickx <v.heynderickx@gmail.com>
2024-04-02 22:23:59 +0200

    rotation de points qui fonctionne

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from matplotlib.patches import Ellipse
 omega = 2*np.pi/10
 fps = 60           # Frames par seconde
 a = 1
-b = 0.2
+b = 0.4
 omega_ellipse = omega*(2*a*b)/(a**2+b**2)  # Vitesse angulaire en radians par seconde
 
 # Création de la figure et des axes
@@ -20,25 +20,37 @@ ellipse = Ellipse((0,0), 2*a, 2*b, fill=False, edgecolor='b')
 ax.add_patch(ellipse)
 
 # Initialisation du point
-point, = ax.plot([], [], 'ro')
+points, = ax.plot([], [], '+', markersize=2, animated=True)
 
 # Fonction d'initialisation de l'animation
 def init():
     ellipse.set_angle(0)
-    point.set_data([], [])
-    return ellipse, point,
+    points.set_data([], [])
+    return ellipse, points,
 
+# Paramètres des points
+# Paramètres des points
+N = 70
+theta = np.linspace(0, 2*np.pi, N)
+x1_0 = np.cos(theta)
+x2_0 = np.sin(theta)
+X1_0, X2_0 = np.meshgrid(x1_0, x2_0)
+def equation(x, y):
+    return (x**2) + (y**2) - 1
+X1_inside = X1_0[equation(X1_0, X2_0) < 0]
+X2_inside = X2_0[equation(X1_0, X2_0) < 0]
 
 # Fonction d'animation
 def animate(frame):
     t = frame / fps
-    x = 0.8*a*np.cos(-omega_ellipse * t)  # Calcul de la position en x
-    y = 0.8*b*np.sin(-omega_ellipse * t)  # Calcul de la position en y
-    x_rotated = np.cos(omega*t)*x - np.sin(omega*t)*y  # Calcul de la position en x après rotation
-    y_rotated = np.sin(omega*t)*x + np.cos(omega*t)*y  # Calcul de la position en y après rotation
-    point.set_data(x_rotated, y_rotated)
+    X1 = (a)*X1_inside * np.cos(-omega_ellipse*t)  - (a)*X2_inside * np.sin(-omega_ellipse*t)  # Calcul de la position en x
+    X2 = (b)*X2_inside * np.cos(-omega_ellipse*t) + (b)*X1_inside * np.sin(-omega_ellipse*t)  # Calcul de la position en y
+    X1_rotated = np.cos(omega*t)*X1 - np.sin(omega*t)*X2  # Calcul de la position en x après rotation
+    X2_rotated = np.sin(omega*t)*X1 + np.cos(omega*t)*X2  # Calcul de la position en y après rotation
+    X1_grid, X2_grid = np.meshgrid(X1_rotated, X2_rotated)
+    points.set_data(X1_rotated, X2_rotated)
     ellipse.set_angle(omega * t * 180 / np.pi)
-    return ellipse, point,
+    return ellipse, points,
 
 
 
